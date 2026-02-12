ce377d19a654ade5c7402b2f8e3b58f29f9a93cb
MalharKt <malharkauthale2019@gmail.com>
2026-02-12 08:06:14 +0000

    Add GitHub Actions pipeline for private EKS

--- backend.tf
+++ backend.tf
@@ -3,7 +3,8 @@ terraform {
     bucket         = "eks-terraform-state-0002"
     key            = "eks/terraform.tfstate"
     region         = "us-east-1"
-    dynamodb_table = "terraform-locks"
+    # dynamodb_table = "terraform-locks"
     encrypt        = true
+    use_lockfile = true
   }
 }
